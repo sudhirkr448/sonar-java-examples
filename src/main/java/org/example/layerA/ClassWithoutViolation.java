cd2f9d68013db4268826a508814e65f28cc2421b
Derrick Fiedler <extern.derrick.fiedler@volkswagen.de>
2014-04-03 13:50:55 +0200

    Added layer to demonstrate the violation 'Architecture Constraint'

---
+++ src/main/java/org/example/layerA/ClassWithoutViolation.java
@@ -0,0 +1,5 @@
+package org.example.layerA;
+
+public class ClassWithoutViolation {
+
+}
---
+++ src/main/java/org/example/layerB/ClassWithViolation.java
@@ -0,0 +1,7 @@
+package org.example.layerB;
+
+import org.example.layerA.ClassWithoutViolation;
+
+public class ClassWithViolation {
+	ClassWithoutViolation c1=new ClassWithoutViolation();
+}
