61eb4ab43252adae97da746dbb91a99cce492923
Derrick Fiedler <derrick.fiedler@yahoo.fr>
2014-05-09 11:09:52 +0200

    new classes added.

---
+++ src/main/java/org/example/ClassA.java
@@ -0,0 +1,18 @@
+package org.example;
+
+/**
+ * Demomstiert das Beispiel für unbenutze Code 
+ */
+public class ClassA {
+
+	private String message;
+	
+    private void displayMessage(){
+    	System.out.println("The message");
+    }
+    
+    public void sayHello() {
+    	System.out.println("Hello Sonar");
+    }
+	
+}
