d69d51d889ee8e55ee78aa656a6c6a9189291f16
Derrick Fiedler <derrick.fiedler@yahoo.fr>
2014-06-04 23:21:24 +0200

    Duplicated code issues added.

--- src/main/java/org/example/ClassA.java
+++ src/main/java/org/example/ClassA.java
@@ -1,5 +1,12 @@
 package org.example;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class ClassA {
 
 	public void execute() {
@@ -25,4 +32,22 @@ public class ClassA {
 		System.out.println("Hello Sonar!");
 		System.out.println("Hello Sonar!");
 	}
+	
+	protected static void copyFileUsingFileStreams(File source, File dest)
+		        throws IOException {
+		    InputStream input = null;
+		    OutputStream output = null;
+		    try {
+		        input = new FileInputStream(source);
+		        output = new FileOutputStream(dest);
+		        byte[] buf = new byte[1024];
+		        int bytesRead;
+		        while ((bytesRead = input.read(buf)) > 0) {
+		            output.write(buf, 0, bytesRead);
+		        }
+		    } finally {
+		        input.close();
+		        output.close();
+		    }
+		}
 }
--- src/main/java/org/example/ClassB.java
+++ src/main/java/org/example/ClassB.java
@@ -1,16 +1,58 @@
 package org.example;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class ClassB {
 
-	public void execute() {
+	public void run() {
 		int x = 10000;
 		x *= 2;
 		x %= 100;
 	}
-	
-	public void doExecute() {
-		int x = 10000;
-		x *= 2;
-		x %= 100;
+
+	protected static void copyFile(File source, File dest) throws IOException {
+		if ((source == null) | (dest == null)) {
+			InputStream input = null;
+			OutputStream output = null;
+			try {
+				input = new FileInputStream(source);
+				output = new FileOutputStream(dest);
+				byte[] buf = new byte[1024];
+				int bytesRead;
+				while ((bytesRead = input.read(buf)) > 0) {
+					output.write(buf, 0, bytesRead);
+				}
+			} finally {
+				input.close();
+				output.close();
+			}
+		}
 	}
+
+	protected static void copyTo(File source, File dest) throws IOException {
+		if ((source == null) | (dest == null)) {
+			throw new IllegalArgumentException("one of both files is NULL");
+		}
+			InputStream input = null;
+			OutputStream output = null;
+			try {
+				input = new FileInputStream(source);
+				output = new FileOutputStream(dest);
+				byte[] buf = new byte[1024];
+				int bytesRead;
+				while ((bytesRead = input.read(buf)) > 0) {
+					output.write(buf, 0, bytesRead);
+				}
+			} finally {
+				input.close();
+				output.close();
+			}
+		
+	}
+
 }
